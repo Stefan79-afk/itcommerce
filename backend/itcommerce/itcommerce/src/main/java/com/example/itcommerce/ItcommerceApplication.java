88d276b483bef4d953e972a7557392ee07a7a4a9
Stefan79-afk <epsilon.number79@gmail.com>
2024-11-19 23:29:14 +0200

    initial commit

---
+++ backend/itcommerce/itcommerce/src/main/java/com/example/itcommerce/ItcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.itcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcommerceApplication.class, args);
+	}
+
+}
