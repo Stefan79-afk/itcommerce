fc65c1339474d2473a8505508fe1f638d6918c5e
Stefan79-afk <epsilon.number79@gmail.com>
2024-12-01 13:04:18 +0200

    rename main application class

--- backend/itcommerce/itcommerce/src/main/java/com/example/itcommerce/Application.java
+++ backend/itcommerce/itcommerce/src/main/java/com/example/itcommerce/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItcommerceApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(ItcommerceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
+
